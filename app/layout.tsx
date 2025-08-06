18d54c3aaf4f6bb51faa9ba822acc5cfb88228d9
Mel <mel@mkodes.com>
2025-08-07 07:52:50 +1200

    added title

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Custom Item Hovers Gen</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex flex-col`}
       >
